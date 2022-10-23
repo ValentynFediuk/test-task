027adb121dbdd615a936bb4b639352868d4b2cd3
Valentyn <sigano802@gmail.com>
2022-10-23 23:19:24 +0300

    [added] sign up form layout

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,25 +1,60 @@
 import styles from './singup.module.scss'
 import ButtonPrimary from '../ui/ButtonPrimary/ButtonPrimary.jsx';
 import {OutlinedInput} from '../ui/OutlinedInput/OutlinedInput.jsx';
+import {useState} from 'react';
 
-const Hero = () => {
+const Signup = () => {
+    const [selectedRadioBtn, setSelectedRadioBtn] = useState('radio-1')
+
+    const isRadioSelected = (value) => selectedRadioBtn === value
 
     return (
         <section className={styles.signup}>
-          <div className='container'>
-              <h2>Working with POST request</h2>
-              <form>
-                <OutlinedInput
-                    type='text'
-                    label='Name'
-                >
-
-                </OutlinedInput>
-              </form>
-              <ButtonPrimary disabled>Sign up</ButtonPrimary>
-          </div>
+            <div className='container'>
+                <h2>Working with POST request</h2>
+                <form>
+                    <OutlinedInput
+                        type='text'
+                        label='Your Name'
+                    ></OutlinedInput>
+                    <OutlinedInput
+                        type='text'
+                        label='Email'
+                    ></OutlinedInput>
+                    <OutlinedInput
+                        type='text'
+                        label='Phone'
+                        helperText='+38 (XXX) XXX - XX - XX'
+                    ></OutlinedInput>
+                    <input
+                        type='radio'
+                        name='radio-btn'
+                        checked={isRadioSelected('radio-1')}
+                        onChange={() => setSelectedRadioBtn('radio-1')}
+                    />
+                    <input
+                        type='radio'
+                        name='radio-btn'
+                        checked={isRadioSelected('radio-2')}
+                        onChange={() => setSelectedRadioBtn('radio-2')}
+                    />
+                    <input
+                        type='radio'
+                        name='radio-btn'
+                        checked={isRadioSelected('radio-3')}
+                        onChange={() => setSelectedRadioBtn('radio-3')}
+                    />
+                    <input
+                        type='radio'
+                        name='radio-btn'
+                        checked={isRadioSelected('radio-4')}
+                        onChange={() => setSelectedRadioBtn('radio-4')}
+                    />
+                </form>
+                <ButtonPrimary disabled>Sign up</ButtonPrimary>
+            </div>
         </section>
     )
 }
 
-export default Hero
+export default Signup
--- src/components/ui/OutlinedInput/OutlinedInput.jsx
+++ src/components/ui/OutlinedInput/OutlinedInput.jsx
@@ -1,10 +1,10 @@
 import styles from './outlined-input.module.scss'
 
-export const OutlinedInput = ({ error, label, type, children, ...props }) => (
+export const OutlinedInput = ({ error, label, type, helperText, ...props }) => (
         <label className={styles.wrapper}>
             <input placeholder={" "} type={type} {...props} />
             <span>{label}</span>
-            {error && <p className={styles.input__error}>{error.message}</p>}
-            {children}
+            {error && <p className={styles.error_text}>{error.message}</p>}
+            <p className={styles.helper_text}>{helperText}</p>
         </label>
 );
